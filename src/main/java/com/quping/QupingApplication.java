8a254a1653fb45f04d1056cf151e1d13ccc9431f
Punny <contactme@gopunny.com>
2024-09-10 22:58:15 +0800

    修改项目结构

--- src/main/java/com/quping/QupingApplication.java
+++ src/main/java/com/quping/QupingApplication.java
@@ -1,5 +1,6 @@
 package com.quping;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date: 2024/9/10 22:23
  */
 @SpringBootApplication
+@MapperScan("com.quping.dao.mapper")
 public class QupingApplication {
     public static void main(String[] args) {
         SpringApplication.run(QupingApplication.class,args);
